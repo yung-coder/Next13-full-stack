8d56b7277d425b56f11d52d7802eaaf0797504c5
yung-coder <chandel.sanjeev5@gmail.com>
2023-07-04 18:25:20 +0530

    upload project

---
+++ app/api/upload/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: "dwiqbu37j",
+  api_key: "797633268635675",
+  api_secret: "6igIcM9CZeU6F9o-DWF01M3yckI",
+});
+
+export async function POST(request: Request) {
+  const { path } = await request.json();
+
+  if (!path) {
+    return NextResponse.json({ message: "Image  path is required" }, {
+      status: 400,
+    });
+  }
+
+  try {
+    const options = {
+      use_filename: true,
+      unique_filename: false,
+      overwrite: true,
+      transformation: [{ width: 1000, height: 752, crop: "scale" }],
+    };
+
+    const result = await cloudinary.uploader.upload(path, options);
+
+    return NextResponse.json(result, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 200 });
+  }
+}
--- libnext/actions.ts
+++ libnext/actions.ts
@@ -1,9 +1,14 @@
-import { createUserMutation, getUserQuery } from "@/graphql";
+import { ProjectForm } from "@/common.types";
+import {
+  createProjectMutation,
+  createUserMutation,
+  getUserQuery,
+} from "@/graphql";
 import { GraphQLClient } from "graphql-request";
 
 const isProduction = process.env.NODE_ENV === "production";
-const apiUrl = process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
-const apiKey =  process.env.NEXT_PUBLIC_GRAFBASE_API_KEY  || ""
+const apiUrl = process.env.NEXT_PUBLIC_GRAFBASE_API_URL || "";
+const apiKey = process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || "";
 const serverUrl = isProduction
   ? process.env.NEXT_PUBLIC_SERVER_URL
   : "http://localhost:3000";
@@ -34,4 +39,39 @@ export const createUser = (name: string, email: string, avatarUrl: string) => {
   };
 
   return makeGraphQLRequest(createUserMutation, variables);
+};
+
+export const uploadImage = async (imagePath: string) => {
+  try {
+    const response = await fetch(`${serverUrl}/api/upload`, {
+      method: "POST",
+      body: JSON.stringify({ path: imagePath }),
+    });
+
+    return response.json();
+  } catch (error) {
+    return error;
+  }
+};
+
+export const createNewProject = async (
+  form: ProjectForm,
+  creatorId: string,
+  token: string,
+) => {
+  const imageUrl = await uploadImage(form.image);
+
+  if (imageUrl.url) {
+    client.setHeader("Authorization", `Bearer ${token}`);
+    const variables = {
+      input: {
+        ...form,
+        image: imageUrl.url,
+        createdBy: {
+          link: creatorId,
+        },
+      },
+    };
+    return makeGraphQLRequest(createProjectMutation, variables);
+  }
 };
