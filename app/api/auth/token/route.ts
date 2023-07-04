790eaac60bf3b4371c846130a8eff30c55f7cd42
yung-coder <chandel.sanjeev5@gmail.com>
2023-07-04 23:21:19 +0530

    error

--- app/api/auth/token/route.ts
+++ app/api/auth/token/route.ts
@@ -1,7 +1,7 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-const secret = "6ae0SZ76aD/6zc3m9f2v9gKCuHTCW5n75RSS8DwmJI4="
+const secret =  process.env.NEXTAUTH_SECRET;
 
 export async function GET(req: NextRequest) {
   const token = await getToken({ req, secret, raw: true });
